6d82abebdad1c020a6ff185ea09bc996dd642775
hyloguy <michael@polyblog.com>
2015-04-26 22:29:11 -0700

    Simplified code and fixed bugs by removing AVAudioPlayer and doing everything with AVAudioEngine

--- PitchImperfect/PlaySoundsViewController.swift
+++ PitchImperfect/PlaySoundsViewController.swift
@@ -11,8 +11,6 @@ import AVFoundation
 
 class PlaySoundsViewController: UIViewController {
     
-    @IBOutlet weak var switchRestartPlaybacks: UISwitch!
-    var audioPlayer:AVAudioPlayer!
     var receivedAudio:RecordedAudio!
     var audioEngine:AVAudioEngine!
     var audioFile:AVAudioFile!
@@ -20,8 +18,6 @@ class PlaySoundsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        audioPlayer = AVAudioPlayer(contentsOfURL: receivedAudio.filePathUrl, error: nil)
-        audioPlayer.enableRate = true
         audioEngine = AVAudioEngine()
         audioFile = AVAudioFile(forReading: receivedAudio.filePathUrl, error: nil)
     }
@@ -30,59 +26,9 @@ class PlaySoundsViewController: UIViewController {
         super.didReceiveMemoryWarning()
     }
     
-    func playAudio(rate: Float) {
-        if switchRestartPlaybacks.on {
-            audioPlayer.currentTime = 0.0
-        }
-        audioPlayer.stop()
-        audioPlayer.rate = rate
-        audioPlayer.play()
-    }
-    
-    func playAudioVariableRate(rate: Float) {
-        audioPlayer.stop()
+    func playAudioWithEffects(rate: Float = 1.0, pitch: Float = 1.0) {
         audioEngine.stop()
         audioEngine.reset()
-        
-        var audioPlayerNode = AVAudioPlayerNode()
-        audioEngine.attachNode(audioPlayerNode)
-        
-        var changeRateEffect = AVAudioUnitTimePitch()
-        changeRateEffect.rate = rate
-        audioEngine.attachNode(changeRateEffect)
-        
-        audioEngine.connect(audioPlayerNode, to: changeRateEffect, format: nil)
-        audioEngine.connect(changeRateEffect, to: audioEngine.outputNode, format: nil)
-        
-        audioPlayerNode.scheduleFile(audioFile, atTime: nil, completionHandler: nil)
-        audioEngine.startAndReturnError(nil)
-        audioPlayerNode.play()
-    }
-    
-    func playAudioWithVariablePitch(pitch: Float) {
-        audioPlayer.stop()
-        audioEngine.stop()
-        audioEngine.reset()
-        
-        var audioPlayerNode = AVAudioPlayerNode()
-        audioEngine.attachNode(audioPlayerNode)
-        
-        var changePitchEffect = AVAudioUnitTimePitch()
-        changePitchEffect.pitch = pitch
-        audioEngine.attachNode(changePitchEffect)
-        
-        audioEngine.connect(audioPlayerNode, to: changePitchEffect, format: nil)
-        audioEngine.connect(changePitchEffect, to: audioEngine.outputNode, format: nil)
-        
-        audioPlayerNode.scheduleFile(audioFile, atTime: nil, completionHandler: nil)
-        audioEngine.startAndReturnError(nil)
-        audioPlayerNode.play()
-    }
-    
-    func playAudioMaster(rate: Float = 1.0, pitch: Float = 1.0) {
-        audioPlayer.stop()
-        audioEngine.stop()
-//        audioEngine.reset()
         
         var audioPlayerNode = AVAudioPlayerNode()
         audioEngine.attachNode(audioPlayerNode)
@@ -102,33 +48,26 @@ class PlaySoundsViewController: UIViewController {
     
     @IBAction func playSlowAudio(sender: UIButton) {
         println("Playing sound slowed down")
-        playAudioMaster(rate: 0.5)
-//        playAudio(0.5)
-//        playAudioVariableRate(0.5)
+        playAudioWithEffects(rate: 0.5)
     }
     
     @IBAction func playFastAudio(sender: UIButton) {
         println("Playing sound sped up")
-        playAudioMaster(rate: 2.5)
-//        playAudio(2.0)
-//        playAudioVariableRate(3.0)
+        playAudioWithEffects(rate: 2.5)
     }
     
     @IBAction func playChipmunkAudio(sender: UIButton) {
         println("Playing sound with chipmunk effect")
-        playAudioMaster(pitch: 1000)
-//        playAudioWithVariablePitch(1000)
+        playAudioWithEffects(pitch: 1000)
     }
     
     @IBAction func playDarthVaderAudio(sender: UIButton) {
         println("Playing sound with Darth Vader effect")
-        playAudioMaster(pitch: -1000)
-//        playAudioWithVariablePitch(-1000)
+        playAudioWithEffects(pitch: -1000)
     }
     
     @IBAction func stopAudioPlaying(sender: UIButton) {
         println("Stopping playback")
-        audioPlayer.stop()
         audioEngine.stop()
         audioEngine.reset()
     }
